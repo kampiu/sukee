ce32052920a012f8c81aa93c717ff2edbba069bd
KAM <1479376017@qq.com>
2022-04-26 12:58:32 +0800

    feat: add plugins

--- src/sukee.ts
+++ src/sukee.ts
@@ -1,4 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+import { rand, randStr, prefixInteger } from './modules/helper'
+
+export { rand, randStr, prefixInteger }
