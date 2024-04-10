7886d4bc78f806ede87822f77ed17384f647b18a
kampiu <1479376017@qq.com>
2024-04-11 01:23:57 +0800

    fix: 调整按钮测试用例存放

--- components/button/__tests__/basic.test.tsx
+++ components/button/__tests__/basic.test.tsx
@@ -1,15 +1,15 @@
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react"
 import React from "react"
-import { Button } from "../components"
+import Button from ".."
 
 describe("Dummy test", () => {
 	it("works if true is truthy", () => {
 		expect(true).toBeTruthy()
 	})
-	
+
 	it("按钮测试", () => {
 		// eslint-disable-next-line react/jsx-filename-extension
-		const { container } = render(<Button>Follow</Button>);
-		expect(container.firstChild).toMatchSnapshot();
+		const { container } = render(<Button>Follow</Button>)
+		expect(container.firstChild).toMatchSnapshot()
 	})
 })
---
+++ components/input/index.en.md
@@ -0,0 +1,20 @@
+---
+category: Components
+group:
+    title: Form
+    order: 2
+title: Input
+subtitle: 输入框
+cover: https://gw.alipayobjects.com/zos/alicdn/fNUKzY1sk/Button.svg
+demo:
+    cols: 2
+---
+
+
+# Input
+
+Through mouse or keyboard input content, it is the most basic form field wrapper.
+
+<code src="./demo/basic.tsx" title="Basic usage." description="Basic usage."></code>
+<code src="./demo/addon.tsx" title="Pre / Post tab" description="Using pre & post tabs example."></code>
+
